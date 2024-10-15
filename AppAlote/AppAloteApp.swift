f80848b2acfc244e396828da300f5e92e608ab90
Gustavo Coutiño <gcoutino04@gmail.com>
2024-10-15 16:52:11 -0600

    Initial Commit

---
+++ AppAlote/AppAloteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppAloteApp.swift
+//  AppAlote
+//
+//  Created by Gustavo Coutiño on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppAloteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppAlote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppAlote
+//
+//  Created by Gustavo Coutiño on 15/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
