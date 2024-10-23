8b0748aea2a1322e2eb5e6efd1d5d450b82b0210
Gustavo Coutiño <gcoutino04@gmail.com>
2024-10-23 12:19:41 -0600

    Changed file structure

---
+++ AppAlote/Components/Triangle.swift
@@ -0,0 +1,20 @@
+//
+//  Triangle.swift
+//  AppAlote
+//
+//  Created by Gustavo Coutiño on 23/10/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.closeSubpath()
+        return path
+    }
+}
--- AppAlote/Views/ContentView.swift
+++ AppAlote/Views/ContentView.swift
@@ -65,17 +65,6 @@ struct ContentView: View {
 }
 
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.closeSubpath()
-        return path
-    }
-}
-
 #Preview {
     ContentView()
 }
