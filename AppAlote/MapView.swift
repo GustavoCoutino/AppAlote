e01ded7bf278245f350f0383572db9db7e112d3e
Gustavo Coutiño <gcoutino04@gmail.com>
2024-10-28 22:31:53 -0600

    Added navbar as component

--- AppAlote/MapView.swift
+++ AppAlote/MapView.swift
@@ -11,58 +11,8 @@ struct MapView: View {
     var body: some View {
         ZStack {
             Map(focused: "Planta baja")
-            VStack {
-                HStack {
-                    Image("home")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 40, height: 40)
-                        .background(
-                            Circle()
-                                .fill(Color(red: 210/255, green: 223/255, blue: 73/255))
-                                .frame(width: 75, height: 75)
-                        )
-                        .padding(.leading, 35)
-                    
-                    Spacer()
-                    Image("location")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 40, height: 40)
-                        .background(
-                            Circle()
-                                .fill(Color(red: 210/255, green: 223/255, blue: 73/255))
-                                .frame(width: 75, height: 75)
-                        )
-                    Spacer()
-                    Image("qr-code")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 40, height: 40)
-                        .background(
-                            Circle()
-                                .fill(Color(red: 210/255, green: 223/255, blue: 73/255))
-                                .frame(width: 75, height: 75)
-                        )
-                    
-                    Spacer()
-                    Image("profile")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 40, height: 40)
-                        .background(
-                            Circle()
-                                .fill(Color(red: 210/255, green: 223/255, blue: 73/255))
-                                .frame(width: 75, height: 75)
-                        )
-                        .padding(.trailing, 35)
-                }
-                .frame(width: UIScreen.main.bounds.size.width)
-                .padding(.vertical, 45)
-                .background(Color.purple)
-                .frame(maxHeight: UIScreen.main.bounds.size.height, alignment: .bottom)
-            }
-            .ignoresSafeArea()
+            Navbar()
+            
             
         }
     }
