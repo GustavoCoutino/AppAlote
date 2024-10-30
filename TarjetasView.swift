d499355f7f1cbf69263f15c1b6cc913e43a0bb42
A01198539 <117104372+A01198539@users.noreply.github.com>
2024-10-30 17:45:18 -0600

    Create TarjetasView.swift

---
+++ TarjetasView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+struct TarjetasView: View {
+    @Binding var selectedCardImage: String
+    
+    let cardImages = ["fondo1", "fondo2", "fondo3", "fondo4", "fondo5","fondo6"]
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 16) {
+                ForEach(cardImages, id: \.self) { cardImage in
+                    ZStack {
+                        Image(cardImage)
+                            .resizable()
+                            .frame(height: 100)
+                            .cornerRadius(10)
+                            .onTapGesture {
+                                selectedCardImage = cardImage
+                            }
+                        
+                        if selectedCardImage == cardImage {
+                            Image(systemName: "checkmark.circle.fill")
+                                .resizable()
+                                .foregroundColor(.green)
+                                .frame(width: 30, height: 30)
+                                .offset(x: 120, y: -30)
+                        }
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+
+#Preview {
+   ContentView()
+}
