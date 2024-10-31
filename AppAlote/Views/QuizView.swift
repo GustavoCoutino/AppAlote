034c165869ecaa6797106482721dc53625500d25
Gustavo Coutiño <gcoutino04@gmail.com>
2024-10-31 13:45:30 -0600

    Added animations to quiz

---
+++ AppAlote/Views/QuizView.swift
@@ -0,0 +1,85 @@
+//
+//  QuizView.swift
+//  AppAlote
+//
+//  Created by Gustavo Coutiño on 23/10/24.
+//
+import SwiftUI
+
+struct QuizView: View {
+    @EnvironmentObject var userManager: UserManager
+    @State private var currentQuestionIndex = 0
+    @State private var selectedAnswer: Int? = nil
+    @State private var navigateToHome = false
+    @StateObject private var routeCalculator = RouteCalculator()
+    private let squareSize = UIScreen.main.bounds.width / 2 - 40
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                let question = QuizData.questions[currentQuestionIndex]
+                
+                ProgressBar(progress: CGFloat(currentQuestionIndex + 1) / CGFloat(QuizData.questions.count))
+                    .padding(.top, 20)
+                
+                Spacer()
+                QuestionTitle(title: question.titulo)
+                
+                Spacer()
+                VStack(spacing: 20) {
+                    ForEach(0..<question.respuestas.count / 2, id: \.self) { row in
+                        HStack(spacing: 10) {
+                            ForEach(0..<2) { column in
+                                let index = row * 2 + column
+                                if index < question.respuestas.count {
+                                    let color = getColor(for: index)
+                                    Text(question.respuestas[index])
+                                        .frame(width: squareSize, height: squareSize)
+                                        .padding()
+                                        .background(color)
+                                        .cornerRadius(10)
+                                        .foregroundColor(.white)
+                                        .onTapGesture {
+                                            selectedAnswer = index
+                                            routeCalculator.recordAnswer(color: color) 
+                                            moveToNextQuestion()
+                                        }
+                                        .padding(1)
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal, 20)
+                
+                Spacer()
+            }
+            .padding()
+        }.navigationBarBackButtonHidden(true)
+    }
+
+    private func getColor(for index: Int) -> Color {
+        let colors: [Color] = [
+            Color(red: 173/255, green: 216/255, blue: 230/255), // Pequeños
+            Color.green, // Pertenezco
+            Color.blue, // Comunico
+            Color(red: 0/255, green: 0/255, blue: 139/255), // Comprendo
+            Color.orange, // Expreso
+            Color.red // Soy
+        ]
+        return colors[index % colors.count]
+    }
+    
+    private func moveToNextQuestion() {
+        if currentQuestionIndex < QuizData.questions.count - 1 {
+            currentQuestionIndex += 1
+            selectedAnswer = nil
+        } else {
+            userManager.setQuizCompleted()
+        }
+    }
+}
+
+#Preview {
+    QuizView()
+}
