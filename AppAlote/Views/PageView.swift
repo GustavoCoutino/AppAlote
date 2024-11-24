6c1968035f6ca3cbf155954f55d359dfe4aa3b1f
miguelmendozajr <miguelmendozaj20@gmail.com>
2024-11-24 15:33:06 -0600

    refactor activity view

--- AppAlote/Views/PageView.swift
+++ AppAlote/Views/PageView.swift
@@ -11,44 +11,50 @@ import SwiftUI
 struct PageView: View {
     var page : Page
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text(page.titulo_es)
-                .font(.headline)
-            
-            Text(page.contenido_es)
-                .font(.body)
-            
-            if let url = URL(string: page.img) {
-                HStack {
-                    Spacer()
-                    AsyncImage(url: url) { phase in
-                        switch phase {
-                        case .empty:
-                            ProgressView()
-                                .frame(width: 300, height: 300)
-                        case .success(let image):
-                            image
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 300, height: 300)
-                        default:
-                            Image(systemName: "photo")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 300, height: 300)
-                                .foregroundColor(.gray)
-                            
+        VStack {
+
+            VStack(alignment: .leading, spacing: 20) {
+                
+                Text(page.titulo_es)
+                    .font(.system(size: 18))
+                    .bold()
+                    .padding(.top, 55)
+                
+                Text(page.contenido_es)
+                    .font(.system(size: 18))
+                    .padding(.bottom, 30)
+                
+                if let url = URL(string: page.img) {
+                    HStack {
+                        AsyncImage(url: url) { phase in
+                            switch phase {
+                            case .empty:
+                                ProgressView()
+                                    .frame(width: 300, height: 300)
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                            default:
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 300, height: 300)
+                                    .foregroundColor(.gray)
+                                
+                            }
                         }
                     }
-                    Spacer()
                 }
             }
-            
+            .frame(maxWidth: 500)
             Spacer()
         }
         .padding()
-        .background(Color(UIColor.systemGreen).opacity(0.1))
+        .frame(maxWidth: .infinity)
+        .background(Color.yellow.opacity(0.1))
         .cornerRadius(10)
+
     }
 }
 
