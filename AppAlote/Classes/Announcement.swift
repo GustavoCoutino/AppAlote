bc22641928dc71089645dbec3d88084c4b96d96c
miguelmendozajr <miguelmendozaj20@gmail.com>
2024-11-24 17:58:49 -0600

    delete _en fields from structs

--- AppAlote/Classes/Announcement.swift
+++ AppAlote/Classes/Announcement.swift
@@ -11,7 +11,5 @@ struct Announcement : Identifiable, Codable {
     let id: Int
     let fecha_creacion: String
     let titulo_es: String
-    let titulo_en: String
     let descripcion_es : String
-    let descripcion_en : String
 }
--- AppAlote/Classes/Zone.swift
+++ AppAlote/Classes/Zone.swift
@@ -10,8 +10,6 @@ struct Zone : Codable {
     let numero_exhibiciones : Int
     let logo : String
     let mensaje_es : String
-    let mensaje_en : String
     let descripcion_es : String
-    let descripcion_en : String
     let multimedia : [String]
 }
